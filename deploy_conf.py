d1008b08bc852c883bf23f4f2908758d9420362f
shaunsephton <shaun@praekeltconsulting.com>
2010-12-08 10:54:18 +0200

    added python exec conf

--- deploy_conf.py
+++ deploy_conf.py
@@ -1,6 +1,9 @@
 # User as which to perform the deploy. Used to setup permissions appropriately and to clone from github.
 AS_USER = 'www-data'
 
+#Python command with which to boostrap Buildout.
+PYTHON_EXEC = 'python2.6'
+
 # Root path in which to perform the deploy. qa/production/current/release structure will be created within this path.
 ROOT_PATH = '/var/praekelt/project_name'
 
